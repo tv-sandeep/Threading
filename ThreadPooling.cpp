0040f8a40df66e9d3ad4e65fcb857b968e501377
tvs <venkatasandeept@gmail.com>
2020-09-05 17:29:55 +0530

    Functionally finished threadpool

--- ThreadPooling.cpp
+++ ThreadPooling.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
 #include <functional>
 #include <vector>
 #include <thread>
 #include <condition_variable>
+#include <queue>
 
 class ThreadPool
 {
 public:
+	using Task = std::function<void()>;
+
 	explicit ThreadPool(std::size_t numThreads)
 	{
 		start(numThreads);
@@ -15,20 +19,40 @@ public:
 		stop();
 	}
 
+	void enqueue(Task task)
+	{
+		{
+			std::unique_lock<std::mutex> lock(m_EventMutex);
+			m_QueueTasks.emplace(std::move(task));
+		}
+		m_EventVar.notify_one();
+	}
+
 private:
 	std::vector<std::thread> m_VectThreads;
 	std::condition_variable m_EventVar;
+
 	std::mutex m_EventMutex;
 	bool m_Stopping = false;
+
+	std::queue<Task> m_QueueTasks;
 	void start(std::size_t numThreads)
 	{
 		m_VectThreads.emplace_back([=]{
 			while (true)
 			{
-				std::unique_lock<std::mutex> lock(m_EventMutex);
-				m_EventVar.wait(lock, [=] {return m_Stopping; });
-				if (m_Stopping)
-					break;
+				Task task;
+				{
+					std::unique_lock<std::mutex> lock(m_EventMutex);
+					m_EventVar.wait(lock, [=] { return m_Stopping || !m_QueueTasks.empty(); });
+				
+					if (m_Stopping && m_QueueTasks.empty())
+						break;
+
+					task = std::move(m_QueueTasks.front());
+					m_QueueTasks.pop();
+				}
+				task();
 			}
 		});
 	}
@@ -47,6 +71,36 @@ private:
 
 int main()
 {
-	ThreadPool pool(36);
+	{
+		ThreadPool pool(5);
+
+		pool.enqueue([] {
+			std::cout << "1\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "2\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "3\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "4\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "5\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "6\n";
+			});
+
+		pool.enqueue([] {
+			std::cout << "7\n";
+			});
+	}
 	return 0;
 }
